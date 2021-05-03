64a3d21dfde4acbe54a735279b9674927178b6d3
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-05-04 00:17:05 +0700

    Create on_boarding_page.dart

---
+++ lib/pages/on_boarding_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_future_jobs/theme.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget backgroundImage() {
+      return Image.asset(
+        'assets/on_boarding.png',
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        fit: BoxFit.fill,
+      );
+    }
+
+    Widget content() {
+      return SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Build Your Next\nFuture Career Like\na Master',
+                style: whiteTextStyle.copyWith(fontSize: 32),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                '18,000 jobs available',
+                style: whiteTextStyle.copyWith(fontWeight: light),
+              ),
+              Spacer(),
+              Center(
+                child: Column(
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 45,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          backgroundColor: whiteColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(66),
+                          ),
+                        ),
+                        child: Text(
+                          'Get Started',
+                          style: purpleTextStyle.copyWith(fontWeight: medium),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 200,
+                      height: 45,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          backgroundColor: transparentColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(66),
+                            side: BorderSide(
+                              color: whiteColor,
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          'Sign In',
+                          style: whiteTextStyle.copyWith(fontWeight: medium),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          backgroundImage(),
+          content(),
+        ],
+      ),
+    );
+  }
+}
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,5 +1,20 @@
-// Note: Colors
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+// Note: Colors
 Color primaryColor = Color(0xff4141A4);
+Color whiteColor = Color(0xffFFFFFF);
+Color transparentColor = Colors.transparent;
+
+// Note: Text Styles
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
+
+// Note: Font Weights
+FontWeight light = FontWeight.w300;
+FontWeight medium = FontWeight.w500;
