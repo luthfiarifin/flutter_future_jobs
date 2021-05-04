a9aaa774d90af1436518e2b5b4d0b0858d57c0b8
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-05-04 23:52:21 +0700

    Create sign_up_page.dart

---
+++ lib/pages/sign_up_page.dart
@@ -0,0 +1,296 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_future_jobs/theme.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController nameController = TextEditingController(text: '');
+  TextEditingController emailController = TextEditingController(text: '');
+  TextEditingController passwordController = TextEditingController(text: '');
+  TextEditingController goalController = TextEditingController(text: '');
+
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Sign Up',
+              style: greyTextStyle.copyWith(fontSize: 16),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Text(
+              'Begin New Journey',
+              style: blackTextStyle.copyWith(
+                fontSize: 24,
+                fontWeight: semiBold,
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget inputPicture() {
+      return Container(
+        width: 120,
+        height: 120,
+        margin: EdgeInsets.only(top: 50),
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: primaryColor,
+          ),
+        ),
+        child: Image.asset(
+          'assets/image_profile.png',
+        ),
+      );
+    }
+
+    Widget inputName() {
+      return Container(
+        margin: EdgeInsets.only(top: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Full Name',
+              style: greyTextStyle.copyWith(fontSize: 16),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextFormField(
+              controller: nameController,
+              cursorColor: primaryColor,
+              onChanged: (value) {
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.only(left: 28, bottom: 20, top: 20),
+                  fillColor: Color(0xffF1F0F5),
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: primaryColor,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget inputEmail() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Email Address',
+              style: greyTextStyle.copyWith(fontSize: 16),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextFormField(
+              controller: emailController,
+              cursorColor: primaryColor,
+              onChanged: (value) {
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.only(left: 28, bottom: 20, top: 20),
+                  fillColor: Color(0xffF1F0F5),
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: primaryColor,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget inputPassword() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Password',
+              style: greyTextStyle.copyWith(fontSize: 16),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextFormField(
+              controller: passwordController,
+              cursorColor: primaryColor,
+              onChanged: (value) {
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.only(left: 28, bottom: 20, top: 20),
+                  fillColor: Color(0xffF1F0F5),
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: primaryColor,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget inputGoal() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Your Goal',
+              style: greyTextStyle.copyWith(fontSize: 16),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextFormField(
+              controller: goalController,
+              cursorColor: primaryColor,
+              onChanged: (value) {
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.only(left: 28, bottom: 20, top: 20),
+                  fillColor: Color(0xffF1F0F5),
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: primaryColor,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget buttonSignUp() {
+      return Container(
+        margin: EdgeInsets.only(top: 40),
+        height: 45,
+        width: double.infinity,
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+              backgroundColor: primaryColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(66))),
+          child: Text(
+            'Sign Up',
+            style: whiteTextStyle.copyWith(
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget buttonBackToSignIn() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 20,
+          bottom: 20,
+        ),
+        child: Center(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Back to Sign In',
+              style: greyTextStyle.copyWith(
+                fontWeight: light,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          child: ListView(
+            children: [
+              header(),
+              inputPicture(),
+              inputName(),
+              inputEmail(),
+              inputPassword(),
+              inputGoal(),
+              buttonSignUp(),
+              buttonBackToSignIn(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// Note: Constant Values
+const double defaultMargin = 24.0;
+
 // Note: Colors
 Color primaryColor = Color(0xff4141A4);
 Color whiteColor = Color(0xffFFFFFF);
 Color transparentColor = Colors.transparent;
+Color greyColor = Color(0xffB3B5C4);
+Color blackColor = Color(0xff272C2F);
 
 // Note: Text Styles
 TextStyle whiteTextStyle = GoogleFonts.poppins(
@@ -15,6 +20,15 @@ TextStyle purpleTextStyle = GoogleFonts.poppins(
   color: primaryColor,
 );
 
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greyColor,
+);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
+
 // Note: Font Weights
 FontWeight light = FontWeight.w300;
 FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w700;
